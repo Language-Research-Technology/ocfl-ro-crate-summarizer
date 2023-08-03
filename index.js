3dea9edb2bc4be40fe4e0406c6bde06bc24d0a02
Peter Sefton <pt@ptsefton.com>
2023-08-04 09:39:34 +1000

    Very preliminary experiments in creating a sqlite db

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const ocfl = require('ocfl-fs');
 const fs = require('fs')
 const {ROCrate} = require('ro-crate');
+const { roCratePreviewFileName } = require('ro-crate/lib/defaults');
 const fileName = process.argv[3]
+
+
 const storage = ocfl.storage({root: process.argv[2], ocflVersion: '1.0'});
 storage.load();
 
@@ -22,32 +25,44 @@ const expandConformsTo = [
 const props = [
     "@id",
     "name",
-    "memberOf"
+    "memberOf",
 ]
 
-const header = [props.join(","), expandConformsTo.map((v)=>"conformsTo_" + v).join(","), expandType.map((v)=>"type_" + v).join(",")].join(",")
+const header = ["path", props.join(","), expandConformsTo.map((v)=>"conformsTo_" + v).join(","), expandType.map((v)=>"type_" + v).join(",")].join(",")
+const asArray = new ROCrate().utils.asArray;
 
-function formatVal(r, p) {
-    // Format a property
-    if (!r[p]) return "";
-    if (r[p]["@id"]) {
-        return `"${r[p]["@id"].replace("\n", "").replace('"', '""')}"`
-    }
-    return `"${r[p].toString().replace('"', '""')}"`
+function clean(s){
+    s = s.toString();
+    return  s.replace('"', '""');
 }
+function formatVal(vals) {
+    console.log(vals)
+    // Format a property
+    if (!vals) return "";
+    vals = vals.map((v) => {
+        if (v["@id"]) {
+            return clean(v["@id"].replace("\n", ""));
+        } else {
+           return  clean(v);
+        }   
+    })
+
+    return vals.join(", ");
+}
+
 async function  main (){
     fs.writeFileSync(fileName, header + "\n")
     for (let obj of await storage.objects()) {
         obj.load();
         const crate = new ROCrate(JSON.parse(await obj.getAsString({logicalPath: "ro-crate-metadata.json"})))
         const r = crate.rootDataset
-        var line = "";
+        var line = `file://${obj.root},`;
 
         for (let p of props) {
-            line += `${formatVal(r, p)},`
+            line += `"${formatVal(crate.utils.asArray(r[p]))}",`
         }
         const conf = crate.utils.asArray(r["conformsTo"]).map((c) => {
-            return c["@id"] ?  c["@id"] : ""
+            return c["@id"] ?  c["@id"] : c
         });
         line += expandConformsTo.map((c) => conf.includes(c) ? "1" : "0").join(",") + ","
         const types = crate.utils.asArray(r["@type"]);
